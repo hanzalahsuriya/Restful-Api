5fdb4557cc5dd88868a1b59e0581b36c89202407
hanzalahsuriya <hanzalah@gmail.com>
2019-02-10 19:26:21 +0000

    add controller

---
+++ Library.Api/Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Controllers
+{
+    public class AuthorsController
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        public IActionResult GetAuthors()
+        {
+            var authors = _libraryRepository.GetAuthors();
+            return new JsonResult(authors);
+        }
+    }
+}
