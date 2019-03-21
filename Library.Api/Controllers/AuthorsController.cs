f32d3f7af87143fe6592fdf9a341f6fdefb7cece
hanzalahsuriya <hanzalah@gmail.com>
2019-03-21 00:19:08 +0000

    adding notes

--- Library.Api/Controllers/AuthorsController.cs
+++ Library.Api/Controllers/AuthorsController.cs
@@ -12,88 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Library.Api.Controllers
 {
 
-    // HTTP Methods
-
-    // GET
-    // url: /api/authors                (to get list of all the authors)
-    // url: /api/authors/{authorId}     (to get a single author)
-
-    // POST
-    // url: /api/authors                (Request payload: Author) to create a new author
-
-    // PUT
-    // url: /api/authors/{authorId}    (Request Payload: Author) to full update 
-
-    // PATCH
-    // url: /api/authors/{authorId}     (Request Payload: JsonPatchDocument of Author) to update selected fields
-
-    // DELETE
-    // url: /api/authors/{authorId}     (To delete author)
-
-    // HEAD
-    // url: /api/authors                (no response ... can be used to testing for validtity)
-    // url: /api/authors/{authorId}
-
-    // OPTIONS
-    // url: /api/authors                (no response ... to tell us about the what methods are available on this uri)
-
-
-    // Status Code
-
-    // 100 
-    // Information and were part of HTTP 1 ... and not used by API
-
-    // 200's .... successfull status codes
-    // 200 - Success
-    // 201 - Created
-    // 204 - No Content
-
-    // 300's ... used for redirection (not much used by api)
-
-    // 400's ... client mistakes
-    // 400 - Bad Request... consumer of the api has sent something wrong.
-    // 401 - Unauthorised (not logged in)
-    // 403 - Forbidden  (logged in but don't have access)
-    // 404 - Not Found (request doesn't exists)
-    // 405 - Method not allowed (trying to send a http requst where a HTTP method is not allowed)
-    // 406 - Not acceptable (when trying to call api with content type which is not acceptable)]
-    // 409 - Conflict (request conflict with current state of the system)
-    // 422 - unprocessable entity - (for e.g. if validation rule failed)
-
-    // 500's ... Server Mistakes
-    // 500 - Internal Server Error
-
-
-    // Method Safety and Idempotency
-
-    // GET
-    // Safe: Yes
-    // Idempotent: Yes
-
-    // OPTIONS
-    // Safe: Yes
-    // Idempotent: Yes
-
-    // HEAD
-    // Safe: Yes
-    // Idempotent: Yes
-
-    // POST
-    // Safe: No
-    // Idempotent: No
-
-    // PUT
-    // Safe: No
-    // Idempotent: Yes
-
-    // PATCH
-    // Safe: No
-    // Idempotent: No
-
-    // DELETE
-    // Safe: No
-    // Idempotent: Yes
-
+    
 
     [Route("api/authors")]
     public class AuthorsController : Controller
@@ -108,18 +27,9 @@ namespace Library.Api.Controllers
         [HttpGet()]
         public IActionResult GetAuthors()
         {
-            //try
-            //{
-                //throw new Exception("something went wrong...");
-                var authorsFromRepository = _libraryRepository.GetAuthors();
-                var authors = AutoMapper.Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepository);
-                return Ok(authors);
-            //}
-            //catch (Exception err)
-            //{
-            //    return StatusCode(500, "Oop's something went wrong");
-            //}
-            
+            var authorsFromRepository = _libraryRepository.GetAuthors();
+            var authors = AutoMapper.Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepository);
+            return Ok(authors);            
         }
 
         [HttpGet("{id}", Name = "GetAuthorById")]
